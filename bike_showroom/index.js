21b4bfcbdb685b2da4ae3f06fb46f52f2ff26239
haricoddle <hariprasad.r@coddletech.com>
2023-11-02 17:25:59 +0530

    added changes

--- bike_showroom/index.js
+++ bike_showroom/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mysql = require('mysql2');
+
 
 const userRouter = require('./routes/user')
 
 app.use(cors());
 
-const {connectDB } = require('./connection')
+const conn = mysql.createConnection( {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'hari'
+});
+conn.connect((err) => {
+    if(err) throw err;
 
-connectDB();
+    console.log('connected to database');
+    
+})
+
+
  
 app.use('/customer', userRouter);
 
